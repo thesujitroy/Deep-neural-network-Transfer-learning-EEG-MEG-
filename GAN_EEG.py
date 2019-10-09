f9d5a71d6deccca8efa10b89a188cc244256c8a9
Sujit Roy <sujitroy1994@hotmail.com>
2019-10-09 17:00:53 +0100

    for EEG d3

--- GAN_EEG.py
+++ GAN_EEG.py
@@ -1,85 +1,80 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Oct  7 11:09:33 2019
+Created on Tue Oct  8 17:12:33 2019
 
 @author: sb00747428
 """
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+
 import tensorflow as tf
 
 import os
 import time
+import numpy as np
 import matplotlib.pyplot as plt
-from IPython.display import clear_output
+from read_data import get_data
+from signalProcessing import run_sig_processing
+from tensorflow import keras
+import pandas as pd
+import matplotlib.pyplot as plt
 
-OUTPUT_CHANNELS = 3
-BUFFER_SIZE = 400
-BATCH_SIZE = 1
-IMG_WIDTH = 32
-IMG_HEIGHT = 40
+"""Collect all the data"""
+def arrange_data(data, labels):
+    output_data = list()
+    output_labels = list()
+    for idx in range(len(data)):
+        for segment in data[idx]:
+            output_data.append(np.expand_dims(segment, axis=2))
+            if labels[idx][0] == 1:
+                output_labels.append(0)
+            else:
+                output_labels.append(1)
+                
+    output_data = np.array(output_data)
+    output_labels = np.array(output_labels)
+    return output_data, output_labels
 
-def resize(input_image, real_image, height, width):
-  input_image = tf.image.resize(input_image, [height, width],
-                                method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
-  real_image = tf.image.resize(real_image, [height, width],
-                               method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+def run_classification(data, labels, session1=(1, 2, 3), session2=(4,5)):
+    
+    
+    for subject in data:
+        # if subject == 'subject1': continue
+        
+        input_data = list()
+        target_labels = list()
+        x_test = list()
+        y_test = list()
+        # combine trials data of target session
+        [input_data.extend(data[subject]["session" + str(idx)]['input data']) for idx in session1]
+        [target_labels.extend(labels[subject]["session" + str(idx)]) for idx in session1]
+        input_data = np.array(input_data)
+        target_labels = np.array(target_labels)
 
-  return input_image, real_image
 
-def normalize(input_image, real_image):
-  input_image = (input_image / 127.5) - 1
-  real_image = (real_image / 127.5) - 1
+        [x_test.extend(data[subject]["session" + str(idx)]['input data']) for idx in session2]
+        [y_test.extend(labels[subject]["session" + str(idx)]) for idx in session2]
+        test_data = np.array(x_test)
+        test_labels = np.array(y_test)
+       
+            
+        train_data, train_labels = arrange_data(input_data, target_labels)
+        test_data, test_labels = arrange_data(x_test, y_test)
+        train_data = train_data.reshape(train_data.shape[0],train_data.shape[1],train_data.shape[2],train_data.shape[4])
+        test_data = test_data.reshape(test_data.shape[0],test_data.shape[1],test_data.shape[2],test_data.shape[4])
+        size_y, size_x = train_data[0].shape[0:2]
+        return train_data, test_data, size_y, size_x, test_labels, train_labels
+    
+"""-----------------"""    
 
-  return input_image, real_image
-
-def random_jitter(input_image, real_image):
-  # resizing to 286 x 286 x 3
-  input_image, real_image = resize(input_image, real_image, 80, 64)
-
-  # randomly cropping to 256 x 256 x 3
-  input_image, real_image = random_crop(input_image, real_image)
-
-  if tf.random.uniform(()) > 0.5:
-    # random mirroring
-    input_image = tf.image.flip_left_right(input_image)
-    real_image = tf.image.flip_left_right(real_image)
-
-  return input_image, real_image
-
-def load_image_train(image_file):
-  input_image, real_image = load(image_file)
-  input_image, real_image = random_jitter(input_image, real_image)
-  input_image, real_image = normalize(input_image, real_image)
-
-  return input_image, real_image
-
-def load_image_test(image_file):
-  input_image, real_image = load(image_file)
-  input_image, real_image = resize(input_image, real_image,
-                                   IMG_HEIGHT, IMG_WIDTH)
-  input_image, real_image = normalize(input_image, real_image)
-
-  return input_image, real_image
-
-''' Input pipeline'''
-train_dataset = tf.data.Dataset.list_files(PATH+'train/*.jpg')
-train_dataset = train_dataset.map(load_image_train,
-                                  num_parallel_calls=tf.data.experimental.AUTOTUNE)
-train_dataset = train_dataset.cache().shuffle(BUFFER_SIZE)
-train_dataset = train_dataset.batch(1)
-
-test_dataset = tf.data.Dataset.list_files(PATH+'test/*.jpg')
-test_dataset = test_dataset.map(load_image_test)
-test_dataset = test_dataset.batch(1)
-'''----------------------------------------------------------------'''
+'''downsample'''
 def downsample(filters, size, apply_batchnorm=True):
   initializer = tf.random_normal_initializer(0., 0.02)
 
   result = tf.keras.Sequential()
   result.add(
-      tf.keras.layers.Conv2D(filters, size, strides=1, padding='same',
+      tf.keras.layers.Conv2D(filters, size, strides=2, padding='same',
                              kernel_initializer=initializer, use_bias=False))
 
   if apply_batchnorm:
@@ -88,18 +83,15 @@ def downsample(filters, size, apply_batchnorm=True):
   result.add(tf.keras.layers.LeakyReLU())
 
   return result
+'''------------------------------------------------------'''
 
-down_model = downsample(3, 4)
-down_result = down_model(tf.expand_dims(inp, 0))
-print (down_result.shape)
-
-
+'''Upsample'''
 def upsample(filters, size, apply_dropout=False):
   initializer = tf.random_normal_initializer(0., 0.02)
 
   result = tf.keras.Sequential()
   result.add(
-    tf.keras.layers.Conv2DTranspose(filters, size, strides=1,
+    tf.keras.layers.Conv2DTranspose(filters, size, strides=2,
                                     padding='same',
                                     kernel_initializer=initializer,
                                     use_bias=False))
@@ -112,11 +104,9 @@ def upsample(filters, size, apply_dropout=False):
   result.add(tf.keras.layers.ReLU())
 
   return result
+"""-----------------"""  
 
-up_model = upsample(3, 4)
-up_result = up_model(down_result)
-print (up_result.shape)
-
+'''Building Generator'''
 def Generator():
   down_stack = [
     downsample(64, 4, apply_batchnorm=False), # (bs, 128, 128, 64)
@@ -168,11 +158,8 @@ def Generator():
 
   return tf.keras.Model(inputs=inputs, outputs=x)
 
-generator = Generator()
-
-gen_output = generator(inp[tf.newaxis,...], training=False)
-plt.imshow(gen_output[0,...])
-
+'''--------------------------------------'''
+'''Building discriminator'''
 def Discriminator():
   initializer = tf.random_normal_initializer(0., 0.02)
 
@@ -201,14 +188,9 @@ def Discriminator():
 
   return tf.keras.Model(inputs=[inp, tar], outputs=last)
 
-discriminator = Discriminator()
-disc_out = discriminator([inp[tf.newaxis,...], gen_output], training=False)
+'''---------------'''
 
-
-LAMBDA = 100
-
-loss_object = tf.keras.losses.BinaryCrossentropy(from_logits=True)
-
+'''Define loss'''
 def discriminator_loss(disc_real_output, disc_generated_output):
   real_loss = loss_object(tf.ones_like(disc_real_output), disc_real_output)
 
@@ -228,33 +210,7 @@ def generator_loss(disc_generated_output, gen_output, target):
 
   return total_gen_loss
 
-generator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
-discriminator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
-
-
-
-
-EPOCHS = 150
-
-def generate_images(model, test_input, tar):
-  # the training=True is intentional here since
-  # we want the batch statistics while running the model
-  # on the test dataset. If we use training=False, we will get
-  # the accumulated statistics learned from the training dataset
-  # (which we don't want)
-  prediction = model(test_input, training=True)
-  plt.figure(figsize=(15,15))
-
-  display_list = [test_input[0], tar[0], prediction[0]]
-  title = ['Input Image', 'Ground Truth', 'Predicted Image']
-
-  for i in range(3):
-    plt.subplot(1, 3, i+1)
-    plt.title(title[i])
-    # getting the pixel values between [0, 1] to plot it.
-    plt.imshow(display_list[i] * 0.5 + 0.5)
-    plt.axis('off')
-  plt.show()
+'''-----------'''
 
 @tf.function
 def train_step(input_image, target):
@@ -276,6 +232,28 @@ def train_step(input_image, target):
                                           generator.trainable_variables))
   discriminator_optimizer.apply_gradients(zip(discriminator_gradients,
                                               discriminator.trainable_variables))
+  
+def generate_images(model, test_input, tar):
+  # the training=True is intentional here since
+  # we want the batch statistics while running the model
+  # on the test dataset. If we use training=False, we will get
+  # the accumulated statistics learned from the training dataset
+  # (which we don't want)
+  prediction = model(test_input, training=True)
+  plt.figure(figsize=(15,15))
+
+  display_list = [test_input[0], tar[0], prediction[0]]
+  title = ['Input Image', 'Ground Truth', 'Predicted Image']
+
+  for i in range(3):
+    plt.subplot(1, 3, i+1)
+    plt.title(title[i])
+    # getting the pixel values between [0, 1] to plot it.
+    plt.imshow(display_list[i] * 0.5 + 0.5)
+    plt.axis('off')
+  plt.show() 
+
+
 
 def fit(train_ds, epochs, test_ds):
   for epoch in range(epochs):
@@ -286,18 +264,51 @@ def fit(train_ds, epochs, test_ds):
       train_step(input_image, target)
 
     clear_output(wait=True)
-    # Test on the same image so that the progress of the model can be
+    # Test on the same image so that the progress of the model can be 
     # easily seen.
     for example_input, example_target in test_ds.take(1):
       generate_images(generator, example_input, example_target)
 
-    # saving (checkpoint) the model every 20 epochs
-    if (epoch + 1) % 20 == 0:
-      checkpoint.save(file_prefix = checkpoint_prefix)
+    
 
     print ('Time taken for epoch {} is {} sec\n'.format(epoch + 1,
                                                         time.time()-start))
+    
+if __name__ == '__main__':
+    # '''
+    OUTPUT_CHANNELS = 3
+    BUFFER_SIZE = 400
+    BATCH_SIZE = 1
+    IMG_WIDTH = 32
+    IMG_HEIGHT = 40
+    EPOCHS = 150
+    data_src = r"Y:\Sujit Roy\data1"
+    labels_src = r"Y:\Sujit Roy\labels1"
+    generator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
+    discriminator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
+    loss_object = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+    LAMBDA = 100
+    #
+    # band_type: 0: band pass feature, 1: AR PSD feature, 2: extend band
+    '''load and prepare data'''
+    data, labels = run_sig_processing(data_src, labels_src, band_type=2)
 
-    fit(train_dataset, EPOCHS, test_dataset)
-    for inp, tar in test_dataset.take(5):
-  generate_images(generator, inp, tar)
+    train_data, test_data, size_y, size_x, test_labels, train_labels = run_classification(data, labels)
+    '''-----------------------------'''
+    '''playing with generator'''
+    generator = Generator()
+
+    gen_output = generator(inp[tf.newaxis,...], training=False)
+    plt.imshow(gen_output[0,...])
+    '''-----------------------------'''
+    
+    '''playing with discriminator'''
+    
+    discriminator = Discriminator()
+    disc_out = discriminator([inp[tf.newaxis,...], gen_output], training=False)
+    plt.imshow(disc_out[0,...,-1], vmin=-20, vmax=20, cmap='RdBu_r')
+    plt.colorbar()
+    
+    '''----------------------------'''
+    
+    fit(train_data, EPOCHS, test_data)
