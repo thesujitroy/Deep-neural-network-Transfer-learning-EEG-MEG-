3eb331a97badd485cba6ede2c25499f37aaa6b2a
Sujit Roy <sujitroy1994@hotmail.com>
2019-10-29 16:20:43 +0000

    Update autoencoder.py

--- autoencoder.py
+++ autoencoder.py
@@ -5,3 +5,36 @@ Created on Tue Oct 29 16:16:37 2019
 @author: sb00747428
 """
 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+from tensorflow.contrib.layers import fully_connected
+
+num_inputs=1280   #40x32 pixels
+num_hid1=640
+num_hid2=320
+num_hid3=num_hid1
+num_output=num_inputs
+lr=0.01
+actf=tf.nn.relu
+
+
+X=tf.placeholder(tf.float32,shape=[None,num_inputs])
+initializer=tf.variance_scaling_initializer()
+
+w1=tf.Variable(initializer([num_inputs,num_hid1]),dtype=tf.float32)
+w2=tf.Variable(initializer([num_hid1,num_hid2]),dtype=tf.float32)
+w3=tf.Variable(initializer([num_hid2,num_hid3]),dtype=tf.float32)
+w4=tf.Variable(initializer([num_hid3,num_output]),dtype=tf.float32)
+
+b1=tf.Variable(tf.zeros(num_hid1))
+b2=tf.Variable(tf.zeros(num_hid2))
+b3=tf.Variable(tf.zeros(num_hid3))
+b4=tf.Variable(tf.zeros(num_output))
+
+hid_layer1=actf(tf.matmul(X,w1)+b1)
+hid_layer2=actf(tf.matmul(hid_layer1,w2)+b2)
+hid_layer3=actf(tf.matmul(hid_layer2,w3)+b3)
+output_layer=actf(tf.matmul(hid_layer3,w4)+b4)
