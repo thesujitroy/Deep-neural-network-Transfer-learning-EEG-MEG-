053504e1a4dde009eb53d71af28d927e54b3c732
Sujit Roy <sujitroy1994@hotmail.com>
2019-10-29 16:55:21 +0000

    Update autoencoder.py

--- autoencoder.py
+++ autoencoder.py
@@ -9,6 +9,9 @@ import tensorflow.contrib.layers as lays
 import numpy as np
 import tensorflow as tf
 
+train_data = np.load('train_data.npy')
+train_labels= np.load('train_labels.npy')
+
 def autoencoder(inputs):
     # encoder
     # 32 x 32 x 1   ->  16 x 16 x 32
@@ -26,7 +29,7 @@ def autoencoder(inputs):
     net = lays.conv2d_transpose(net, 1, [5, 5], stride=2, padding='SAME', activation_fn=tf.nn.tanh)
     return net
 
-ae_inputs = tf.placeholder(tf.float32, (None, 32, 32, 1))  # input to the network (MNIST images)
+ae_inputs = tf.placeholder(tf.float32, (None, 40, 32, 1))  # input to the network (MNIST images)
 ae_outputs = autoencoder(ae_inputs)  # create the Autoencoder network
 # calculate the loss and optimize the network
 loss = tf.reduce_mean(tf.square(ae_outputs - ae_inputs))  # claculate the mean square error loss
@@ -38,20 +41,20 @@ batch_size = 40  # Number of samples in each batch
 epoch_num = 5     # Number of epochs to train the network
 lr = 0.001        # Learning rate
 # read MNIST dataset
-mnist = input_data.read_data_sets("MNIST_data", one_hot=True)
+
 # calculate the number of batches per epoch
-batch_per_ep = mnist.train.num_examples // batch_size
+batch_per_ep = train_data.shape[0] // batch_size
 with tf.Session() as sess:
     sess.run(init)
     for ep in range(epoch_num):  # epochs loop
         for batch_n in range(batch_per_ep):  # batches loop
-            batch_img, batch_label = mnist.train.next_batch(batch_size)  # read a batch
-            batch_img = batch_img.reshape((-1, 28, 28, 1))               # reshape each sample to an (28, 28) image
-            batch_img = resize_batch(batch_img)                          # reshape the images to (32, 32)
+            batch_img, batch_label = train_data.next_batch(batch_size)  # read a batch
+            batch_img = batch_img.reshape((-1, 40, 32, 1))               # reshape each sample to an (28, 28) image
+            #batch_img = resize_batch(batch_img)                          # reshape the images to (32, 32)
             _, c = sess.run([train_op, loss], feed_dict={ae_inputs: batch_img})
             print('Epoch: {} - cost= {:.5f}'.format((ep + 1), c))
     # test the trained network
-    batch_img, batch_label = mnist.test.next_batch(50)
+    batch_img, batch_label = train_data.next_batch(50)
     batch_img = resize_batch(batch_img)
     recon_img = sess.run([ae_outputs], feed_dict={ae_inputs: batch_img})[0]
     # plot the reconstructed images and their ground truths (inputs)
